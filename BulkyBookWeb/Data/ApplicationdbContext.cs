1fa65de0f93fd8a9baf64faaac0e41f56922ab30
manan-bedi2908 <mananbedilps@gmail.com>
2022-02-01 15:25:14 +0530

    add files

---
+++ BulkyBookWeb/Data/ApplicationdbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationdbContext :DbContext
+    {
+        public ApplicationdbContext(DbContextOptions<ApplicationdbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
